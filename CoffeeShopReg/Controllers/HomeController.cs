2c4c69b02a86ef38c9e5f6b48d31b1990564c475
kmerck88 <Kevin.Merck88@gmail.com>
2022-09-19 19:03:15 -0400

    adding a the the final touches

---
+++ CoffeeShopReg/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CoffeeShopReg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        [HttpPost]
+       
+        public ActionResult form1(int txtId, string txtName, string chkAddon)
+        {
+            ViewBag.Id = txtId;
+            ViewBag.Name = txtName;
+            if (chkAddon != null)
+                ViewBag.Addon = "Selected";
+            else
+                ViewBag.Addon = "Not Selected";
+
+            return View("Index");
+        }
+    }
+    
+}
---
+++ CoffeeShopReg/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShopReg.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
